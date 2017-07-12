19194ea4b3d7fc0166108e1c26c8d2a26fa13a59
Mario de Biase <debiasej@gmail.com>
2017-07-12 11:36:12 +0200

    Add a mock for side menu content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,18 +4,31 @@ import { StatusBar } from '@ionic-native/status-bar'
 import { SplashScreen } from '@ionic-native/splash-screen'
 
 import { HomePage } from '../pages/home/home'
+import { GetSideMenuMock } from '../data/datasource/GetSideMenuMock'
+
 @Component({
   templateUrl: 'app.html',
+  providers: [GetSideMenuMock]
 })
 export class MyApp {
   rootPage:any = HomePage
+  sideMenuContent:Array<any>
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  @ViewChild('content') content: Nav
+
+  constructor(
+    platform: Platform,
+    statusBar: StatusBar,
+    splashScreen: SplashScreen,
+    service: GetSideMenuMock
+  ) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault()
       splashScreen.hide()
+
+      this.sideMenuContent = service.getSideMenu()
     });
   }
 }
