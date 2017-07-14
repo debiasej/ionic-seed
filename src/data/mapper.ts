d2366d8304896217b571e58d866911a46a5a7149
Oscar Fernandez Bravo <oscarfernandezbravo@hotmail.com>
2017-07-14 08:30:12 +0200

    Move mapper to data

---
+++ src/data/mapper.ts
@@ -0,0 +1,18 @@
+import SideMenuBo from '../domain/entities/sideMenuBo'
+
+export class Mapper {
+
+  sideMenuDtotoSideMenuBo (dto: Array<any>) : Array<SideMenuBo> {
+    var result : Array<SideMenuBo> = new Array<SideMenuBo>()
+
+    for (let element of dto) {
+        var sideMenuBo : SideMenuBo  = new SideMenuBo()
+        sideMenuBo.title = element["titulo"]
+        sideMenuBo.nextPage = element["destino"]
+        sideMenuBo.pageType = element["tipo"] 
+        result.push(sideMenuBo)  
+    }
+
+    return result
+  }
+}
