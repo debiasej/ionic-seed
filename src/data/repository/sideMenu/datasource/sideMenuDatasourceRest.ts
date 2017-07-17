6db871d46e31dd8b2deeb4e756ee2a078ab6f9ab
Oscar Fernandez Bravo <oscarfernandezbravo@hotmail.com>
2017-07-17 09:51:50 +0200

    Load Http module without angular injection

--- src/data/repository/sideMenu/datasource/sideMenuDatasourceRest.ts
+++ src/data/repository/sideMenu/datasource/sideMenuDatasourceRest.ts
@@ -1,4 +1,9 @@
 import { injectable } from 'inversify'
+import { ReflectiveInjector } from '@angular/core'
+import { Http, BrowserXhr, RequestOptions, ResponseOptions, ConnectionBackend,
+  XSRFStrategy, BaseRequestOptions, BaseResponseOptions, XHRBackend, CookieXSRFStrategy  } from '@angular/http'
+import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/toPromise'
 
 import { SideMenuDatasource } from './sideMenuDatasource'
 import SideMenuBo from '../../../../domain/entities/sideMenuBo'
@@ -9,42 +14,43 @@ import Mapper from '../../../dto/sideMenu/mapper/sideMenuMapper'
 @injectable()
 class SideMenuDatasourceRest implements SideMenuDatasource {
 
+  http: Http
   mapper: Mapper
 
   constructor() {
     this.mapper = new Mapper()
+
+    let injector = ReflectiveInjector.resolveAndCreate([
+        Http,
+        BrowserXhr,
+        {provide: RequestOptions, useClass: BaseRequestOptions},
+        {provide: ResponseOptions, useClass: BaseResponseOptions},
+        {provide: ConnectionBackend, useClass: XHRBackend},
+        {provide: XSRFStrategy, useFactory: () => new CookieXSRFStrategy()},
+    ]);
+
+    this.http = injector.get(Http);
   }
 
-  async getSideMenuItems() : Promise<Array<SideMenuBo>> {
+  getSideMenuItems() : Promise<Array<SideMenuBo>> {
 
-    var itemsDto: Array<SideMenuDto> = new Array<SideMenuDto>()
-    var dto: SideMenuDto = new SideMenuDto()
-
-    dto['titulo'] = 'Portada'
-    dto['destino'] = 'PortadaPage'
-    dto['tipo'] = 'layoutDetail'
-    dto['tabBar'] = { visible: true, orden: 0 }
-    dto['sideBar'] =  { visible: true, orden: 0 }
-    itemsDto.push(dto)
-
-    var itemsBo:  Array<SideMenuBo> = this.mapToBo(itemsDto)
-    itemsBo = await this.delay(5000, itemsBo)
-
-    return itemsBo
+    var itemsBo: Array<SideMenuBo> 
+    var promise: Promise<Array<SideMenuDto>> = this.http.get('https://demo5902092.mockable.io/v2.9/1916622435261787/leads')
+      .map(res => {
+        return res.json()
+      })
+      .toPromise()
+      .catch(err => {
+        console.error('Http error!')
+    })
+    
+    return promise.then(itemsDto => itemsBo = this.mapToBo(itemsDto))
   }
 
   private mapToBo(itemsDto: Array<SideMenuDto>) : Array<SideMenuBo> {
     return this.mapper.sideMenuDtotoSideMenuBo(itemsDto)
   }
-
-  private delay(milliseconds: number, items: Array<SideMenuBo>): Promise<Array<SideMenuBo>> {
-    return new Promise<Array<SideMenuBo>>(resolve => {
-        setTimeout(() => {
-            resolve(items)
-        }, milliseconds)
-    })
-  }
-
+  
 }
 
 export default SideMenuDatasourceRest
